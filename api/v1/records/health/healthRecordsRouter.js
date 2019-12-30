52257899acd38a8970ee5b0646cf99da1703a641
Ayodeji Fakunle <ayodejisamuelfakunle@gmail.com>
2019-12-30 16:53:13 +0100

    'COMMIT'

--- api/v1/records/health/healthRecordsRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authUser = require(".../users/usersController.js");
-const HealthRecordsController = require("./healthRecordsController.js");
-
-/*router.route("/user/:id")
-	.get(authUser.userAuthenticate, HealthRecordsController.getHealthRecords);
-	.patch(authUser.userAuthenticate, HealthRecordsController.updateHealthRecords);
