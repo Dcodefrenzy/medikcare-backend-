c83ab70da0798caa743b9e0f0053156a54fe292e
Ayodeji Fakunle <ayodejisamuelfakunle@gmail.com>
2020-01-06 10:27:48 +0100

    changed cors settings

--- server.js
+++ server.js
@@ -9,9 +9,15 @@ const api = require("./api/v1/api.js");
 require('dotenv').config()
 
 
-const coreOptions = {
-	origin: "http://localhost:3000", 
-	optionsSuccessStatus: 200
+const whitelist = ['https://www.medikcare.com/','http://www.medikcare.com/', "http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 }
 
 app.use(bodyParser.json({limit: '10mb', extended: true}))
